bcb6e702709b86e803ac2e32db8f3b00a666f450
Mark-A-A <markawai01@gmail.com>
2019-05-23 23:45:31 -0400

    question 1 string reversal done

--- exercises/1-reversestring/index.js
+++ exercises/1-reversestring/index.js
@@ -6,6 +6,10 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  return str.split("").reduce((acc, val, i)=>{
+    return val + acc
+  },'')
+}
 
 module.exports = reverse;
