7d29c111831f0def6e20c2709781fba46d674f84
Mark-A-A <markawai01@gmail.com>
2019-05-23 23:40:22 -0400

    #2 palindrome solved

--- exercises/2-palindrome/index.js
+++ exercises/2-palindrome/index.js
@@ -7,6 +7,11 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  for (let i = 0; i < str.length-1/2; i++) {
+    if (str[i] !== str[str.length-i-1]) return false  
+  }
+  return true
+}
 
 module.exports = palindrome;
